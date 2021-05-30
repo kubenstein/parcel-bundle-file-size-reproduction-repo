f176393c7d38a9fa536574fe67caa2bb2c23fb4a
Jakub Niewczas <niewczas.jakub@gmail.com>
2021-05-30 12:44:53 +0900

    copy demo files

---
+++ src/frontend/components/App/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="helloWorld">
+    <h1>Hello {{ who }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      who: "loading...",
+    };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.who = "Parcel";
+    }, 1000);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.helloWorld {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
